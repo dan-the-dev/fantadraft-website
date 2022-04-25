9208ca91fea726055643a01b2a15031d7677763b
dan-the-dev <daniele.scillia@gmail.com>
2022-04-25 14:54:48 +0200

    feat: first texts

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,19 +16,15 @@ const Home: NextPage = () => {
               <nav className="flex items-center w-full h-24 select-none" x-data="{ showMenu: false }">
                   <div className="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
                       <a href="#_" className="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
-                          <span className="p-1 text-xl font-black leading-none text-white select-none"><span>tails</span><span className="text-indigo-300">.</span></span>
+                          <span className="p-1 text-xl font-black leading-none text-gray-200 select-none"><span>tails</span><span className="text-indigo-300">.</span></span>
                       </a>
                       <div className="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-gray-900 bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex">
                           <div className="flex-col w-full h-auto h-full overflow-hidden bg-white rounded-lg select-none md:bg-transparent md:rounded-none md:relative md:flex md:flex-row md:overflow-auto">
                               <div className="flex flex-col items-center justify-center w-full h-full mt-12 text-center text-indigo-700 md:text-indigo-200 md:w-2/3 md:mt-0 md:flex-row md:items-center">
-                                  <a href="#" className="inline-block px-4 py-2 mx-2 font-medium text-left text-indigo-700 md:text-white md:px-0 lg:mx-3 md:text-center">Home</a>
-                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Features</a>
-                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Blog</a>
-                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Contact</a>
-                              </div>
-                              <div className="flex flex-col items-center justify-end w-full h-full pt-4 md:w-1/3 md:flex-row md:py-0">
-                                  <a href="#_" className="w-full pl-6 mr-0 text-indigo-200 hover:text-white md:pl-0 md:mr-3 lg:mr-5 md:w-auto">Sign In</a>
-                                  <a href="#_" className="inline-flex items-center justify-center px-4 py-2 mr-1 text-base font-medium leading-6 text-indigo-600 whitespace-no-wrap transition duration-150 ease-in-out bg-white border border-transparent rounded-full hover:bg-white focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700">Sign Up</a>
+                                  <a href="#" className="inline-block px-4 py-2 mx-2 font-medium text-left text-indigo-700 md:text-gray-200 md:px-0 lg:mx-3 md:text-center">Home</a>
+                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-gray-200 lg:mx-3 md:text-center">Features</a>
+                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-gray-200 lg:mx-3 md:text-center">Blog</a>
+                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-gray-200 lg:mx-3 md:text-center">Contact</a>
                               </div>
                           </div>
                       </div>
@@ -44,12 +40,12 @@ const Home: NextPage = () => {
               </nav>
               <div className="container py-32 mx-auto text-center sm:px-4">
 
-                  <h1 className="text-4xl font-extrabold leading-10 tracking-tight text-white sm:text-5xl sm:leading-none md:text-6xl xl:text-7xl"><span className="block">Scopri un fantacalcio</span> <span className="relative inline-block mt-3 text-white">tutto nuovo</span></h1>
+                  <h1 className="text-4xl font-extrabold leading-10 tracking-tight text-gray-200 sm:text-5xl sm:leading-none md:text-6xl xl:text-7xl"><span className="block">Scopri un fantacalcio</span> <span className="relative inline-block mt-3 text-gray-200">tutto nuovo</span></h1>
                   <div className="max-w-lg mx-auto mt-6 text-sm text-center text-indigo-200 md:mt-12 sm:text-base md:max-w-3xl md:text-lg xl:text-xl">Stanco del solito Fantacalcio? Scopri Fantadraft! Aggiungendo le dinamiche del Draft stile Magic abbiamo creato un esperienza di gioco inedita e divertentissima!</div>
                   <div className="relative flex items-center max-w-lg mx-auto mt-12 overflow-hidden text-center rounded-full">
                       <input type="text" name="email" placeholder="La tua email" className="w-full h-12 px-6 py-2 font-medium text-indigo-800 focus:outline-none" />
                       <span className="relative top-0 right-0 block">
-                          <button type="button" className="inline-flex items-center w-56 h-12 px-8 text-base font-bold leading-6 text-white transition duration-150 ease-in-out bg-indigo-400 border border-transparent hover:bg-indigo-700 focus:outline-none active:bg-indigo-700">
+                          <button type="button" className="inline-flex items-center w-56 h-12 px-8 text-base font-bold leading-6 text-gray-200 transition duration-150 ease-in-out bg-indigo-400 border border-transparent hover:bg-indigo-700 focus:outline-none active:bg-indigo-700">
                               Gioca in anteprima
                           </button>
                       </span>
@@ -94,10 +90,10 @@ const Home: NextPage = () => {
           </div>
       </section>
 
-      <section className="text-gray-700 bg-white body-font">
+      <section className="text-gray-200 bg-indigo-600 body-font">
           <div className="container flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
-              <a href="#_" className="text-xl font-black leading-none text-gray-900 select-none logo">tails<span className="text-indigo-600">.</span></a>
-              <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">© 2021 Tails - Tailwindcss Page Builder
+              <a href="#_" className="text-xl font-black leading-none text-gray-200 select-none logo">tails<span className="text-indigo-600">.</span></a>
+              <p className="mt-4 text-sm text-gray-200 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">© 2021 Tails - Tailwindcss Page Builder
               </p>
               <span className="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
                   <a href="#" className="text-gray-400 hover:text-gray-500">
