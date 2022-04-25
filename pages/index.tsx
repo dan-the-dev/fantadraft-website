003e91a12b96e159e0de593ac03f1af5458da2ea
dan-the-dev <daniele.scillia@gmail.com>
2022-04-25 14:57:42 +0200

    feat: first texts

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,28 +14,8 @@ const Home: NextPage = () => {
       <section className="w-full px-3 antialiased bg-indigo-600 lg:px-6">
           <div className="mx-auto max-w-7xl">
               <nav className="flex items-center w-full h-24 select-none" x-data="{ showMenu: false }">
-                  <div className="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
-                      <a href="#_" className="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
-                          <span className="p-1 text-xl font-black leading-none text-gray-200 select-none"><span>tails</span><span className="text-indigo-300">.</span></span>
-                      </a>
-                      <div className="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-gray-900 bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex">
-                          <div className="flex-col w-full h-auto h-full overflow-hidden bg-white rounded-lg select-none md:bg-transparent md:rounded-none md:relative md:flex md:flex-row md:overflow-auto">
-                              <div className="flex flex-col items-center justify-center w-full h-full mt-12 text-center text-indigo-700 md:text-indigo-200 md:w-2/3 md:mt-0 md:flex-row md:items-center">
-                                  <a href="#" className="inline-block px-4 py-2 mx-2 font-medium text-left text-indigo-700 md:text-gray-200 md:px-0 lg:mx-3 md:text-center">Home</a>
-                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-gray-200 lg:mx-3 md:text-center">Features</a>
-                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-gray-200 lg:mx-3 md:text-center">Blog</a>
-                                  <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-gray-200 lg:mx-3 md:text-center">Contact</a>
-                              </div>
-                          </div>
-                      </div>
-                      <div className="absolute right-0 z-50 flex flex-col items-end w-10 h-10 p-2 mr-4 rounded-full cursor-pointer md:hidden hover:bg-gray-900 hover:bg-opacity-10">
-                          <svg className="w-6 h-6" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" x-cloak="">
-                              <path d="M4 6h16M4 12h16M4 18h16"></path>
-                          </svg>
-                          <svg className="w-6 h-6" x-show="showMenu" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" x-cloak="">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                          </svg>
-                      </div>
+                  <div className="relative flex flex-wrap items-center justify-center w-full h-24 mx-auto font-medium md:justify-center">
+                    <span className="p-1 text-xl font-black leading-none text-gray-200 select-none"><span>FANTADRAFT</span></span>
                   </div>
               </nav>
               <div className="container py-32 mx-auto text-center sm:px-4">
