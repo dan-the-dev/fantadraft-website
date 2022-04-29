a448c3aaa4611a18639e5886d8e2c0e3fcce1cc6
dan-the-dev <daniele.scillia@gmail.com>
2022-04-29 12:33:11 +0200

    feat: fixed header with language

--- components/header.tsx
+++ components/header.tsx
@@ -1,37 +1,41 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { ChevronDownIcon } from '@heroicons/react/outline'
+
+type locale = 'it' | 'en'
 
 const Header = () => {
   const router = useRouter()
 
-  const changeLocale = (locale: 'it' | 'en', withClick: boolean) => {
-    if (!withClick){
-      return
-    }
+  const changeLocale = (locale: locale) => {
     router.push(router.asPath, router.asPath, { locale: locale })
     router.reload()
   }
 
-  const itFlag = (withClick: boolean = true) => <><Image src={'/flags/it.png'} width={'25'} height={'20'} onClick={() => {changeLocale('it', withClick)}} className='cursor-pointer'/></>
-  const enFlag = (withClick: boolean = true) => <><Image src={'/flags/en.png'} width={'25'} height={'20'} onClick={() => {changeLocale('en', withClick)}} className='cursor-pointer'/></>
+  const flag = (locale: locale, active: boolean) => <>{!active && <p>{locale === 'it' ? 'Italiano' : 'English'}</p>}<Image src={'/flags/'+locale+'.png'} width={'25'} height={'20'} onClick={() => {if (active) {return}; changeLocale(locale)}} className='cursor-pointer'/></>
+  const itFlag = (active: boolean) => flag('it', active)
+  const enFlag = (active: boolean) => flag('en', active)
 
   const active = () => {
-    return router.locale === 'it' ? itFlag(false) : enFlag(false)
+    return router.locale === 'it' ? itFlag(true) : enFlag(true)
   }
 
   const inactive = () => {
-    return router.locale === 'it' ? enFlag() : itFlag()
+    return router.locale === 'it' ? enFlag(false) : itFlag(false)
   }
 
   return (
     <>
       <section className="w-full px-3 antialiased bg-white lg:px-6">
         <nav className="flex items-center w-full h-24 select-none" x-data="{ showMenu: false }">
-            <div className="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
+            <div className="relative flex flex-wrap items-center justify-start md:justify-center w-full h-24 mx-auto font-medium">
                 <Image src={'/logo.jpg'} width={'240'} height={'80'} onClick={() => {router.push('/')}} className='cursor-pointer'/>
-                <div className="dropdown dropdown-end bg-transparent border-none">
-                  <label tabIndex={0} className="btn m-1 bg-transparent border-none">{active()}</label>
-                  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-16">
+                <div className="dropdown dropdown-end bg-transparent border-none absolute right-0">
+                  <label tabIndex={0} className="btn m-1 bg-transparent border-none">
+                    {active()}
+                    <ChevronDownIcon className='w-4 text-primary ml-1'/>  
+                  </label>
+                  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-32">
                     <li><a>{inactive()}</a></li>
                   </ul>
                 </div>
