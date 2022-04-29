4dbf3a6a8ed9e9fb5410c31d90363428d64c9f0e
dan-the-dev <daniele.scillia@gmail.com>
2022-04-29 20:54:19 +0200

    fix: fixed soccer in football

--- pages/blog/cos-e-fantadraft-la-rivoluzione-del-fantacalcio.tsx
+++ pages/blog/cos-e-fantadraft-la-rivoluzione-del-fantacalcio.tsx
@@ -50,12 +50,12 @@ const Blog: NextPage = () => {
     const enContent = () => {
         return <>
             <p>
-                Fantadraft is a totally new game, never seen before. We imagined a new fantasy soccer, in which every week you can make your own strategy to create your formation and try to get as many points as possible to win prizes (and brag about it to your friends!).
+                Fantadraft is a totally new game, never seen before. We imagined a new fantasy football game, in which every week you can make your own strategy to create your formation and try to get as many points as possible to win prizes (and brag about it to your friends!).
             </p>
             <br />
             <h2 className="text-lg font-bold">Fantacalcio + Draft</h2>
             <p>
-                Fantasy soccer is really special thanks to the auction: a unique and inimitable social moment. At the same time, the auction is also the biggest limitation of the game: if something goes wrong that day, you will pay for it for the whole year.
+                What makes fantasy football really special is the initial auction: a unique and inimitable social moment. At the same time, the auction is also the biggest limitation of the game: if something goes wrong that day, you will pay for it for the whole year.
             </p>
             <br />
             <p>
@@ -67,7 +67,7 @@ const Blog: NextPage = () => {
             </p>
             <br />
             <p>
-            With Fantadraft we want to create a game that is always fun, for everyone, every single day of the soccer season, until the last one. A game where every week is fun to choose the players and the final lineup. By renouncing to the auction, we distribute the fun of those 6 hours (if it goes well!) along all the game days.
+            With Fantadraft we want to create a game that is always fun, for everyone, every single day of the football season, until the last one. A game where every week is fun to choose the players and the final lineup. By renouncing to the auction, we distribute the fun of those 6 hours (if it goes well!) along all the game days.
             </p>
             <div className="flex justify-center py-5 px-5 md:px-0">
                 <Image src={'/kickstarter.png'} width={'300px'} height={'100px'} layout={'fixed'}/>
